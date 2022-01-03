2cef673bee5435013677a2120de5e1f8c21ad450
Pedram <pedahd@gmail.com>
2022-01-03 18:04:34 -0500

    Adds reset lang model

---
+++ reset-lang-model.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+model=$1
+key=$2
+
+
+
+curl -X POST -u "apikey:$key" "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/61b4ec92-bb32-4c6e-878b-ac7030a7f439/v1/customizations/$model/reset"
